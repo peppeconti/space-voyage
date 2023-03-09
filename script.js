e05bf6654b0f9e15ad12f6d06ea5296787357239
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-03-09 13:32:52 +0100

    update

--- script.js
+++ script.js
@@ -6,13 +6,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('load', () => {
 
+        let meteor_tl;
+
         const land = document.querySelector('.land').contentDocument.getElementById('land');
+        const title = document.querySelector('.title');
+        const spaceship = document.querySelector('.spaceship');
+        const meteor_wrapper = document.querySelector('.meteor-wrapper');
+        const meteor = document.querySelector('.meteor');
 
-        //const spaceship = document.querySelector('.spaceship').contentDocument.getElementById('spaceship');
-
+        console.log(meteor.dataset)
 
         gsap.set(land, {
             y: '100%'
+        });
+
+        gsap.set(meteor_wrapper, {
+            width: meteor.offsetWidth / 4,
+            height: meteor.offsetHeight
         });
 
         gsap.to(land, {
@@ -26,64 +36,76 @@ document.addEventListener('DOMContentLoaded', () => {
             y: '0%',
         });
 
-        gsap.to('.title', {
+        gsap.to(title, {
             scrollTrigger: {
                 trigger: 'main',
                 start: 'top top',
                 end: '15% center',
                 scrub: true,
+                onEnter() {
+                    title.classList.remove('blink');
+                },
+                onLeaveBack() {
+                    title.classList.add('blink');
+                }
                 //markers: true
             },
             top: '0%',
             opacity: 0
         });
 
-        gsap.to('.spaceship', {
+        gsap.to(spaceship, {
             scrollTrigger: {
                 trigger: 'main',
                 start: 'top top',
                 end: '20% center',
                 scrub: true,
-                markers: true
+                //markers: true
             },
             opacity: 1,
         });
 
-        gsap.to('.spaceship', {
+        gsap.to(spaceship, {
             scrollTrigger: {
                 trigger: 'main',
                 start: 'top top',
                 end: '50% center',
                 scrub: true,
-                markers: true
+                //markers: true
             },
             top: '50%',
             transform: 'translate(-50%, -50%)'
         });
 
+        // ANIMATION
+
+        meteor_tl = new TimelineMax({ repeat: -1, paused: true, });
+
+        meteor_tl.to(meteor, +meteor.dataset.speed, {
+            x: + meteor.offsetWidth / (+meteor.dataset.frames / (+meteor.dataset.frames - 1)),
+            ease: SteppedEase.config(+meteor.dataset.frames - 1)
+        });
+
+        const playMeteor = () => {
+            meteor_tl.play();
+        };
+
+        const reverseMeteor = () => {
+            meteor_tl.reverse();
+        };
+
+        const pauseMeteor = () => {
+            meteor_tl.pause();
+        };
+        
+        ScrollTrigger.observe({
+            target: window,
+            type: 'scroll',
+            onUp: () => requestAnimationFrame(reverseMeteor),
+            onDown: () => requestAnimationFrame(playMeteor),
+            onStop: () => requestAnimationFrame(pauseMeteor),
+          });
+
     }, false);
 
 });
-
-
-/*gsap.to('.spaceship', {
-            scrollTrigger: {
-                trigger: 'main',
-                start: 'top top',
-                end: '75% center',
-                scrub: true,
-                markers: true
-            },
-            top: '30%',
-            transform: 'translate(-50%, -50%)'
-        });
-
-        gsap.to('.spaceship', {
-            scrollTrigger: {
-                trigger: 'main',
-                start: '80% center',
-                scrub: 4,
-                markers: true
-            },
-            y: 280
-        });*/
