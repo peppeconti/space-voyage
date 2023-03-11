f84bfcbb41121723a472e0c6380afb0b1bfc5ab2
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-03-11 12:22:10 +0100

    update

--- script.js
+++ script.js
@@ -15,42 +15,50 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         const land = document.querySelector('.land-wrapper');
-        const land_1 = document.querySelector('.land').contentDocument.getElementById('land-1');
-        const land_2 = document.querySelector('.land').contentDocument.getElementById('land-2');
+        const departure = document.querySelector('.land').contentDocument.getElementById('land-1');
+        const arrive = document.querySelector('.land').contentDocument.getElementById('land-2');
         const spaceship_wrapper = document.querySelector('.spaceship-wrapper');
         const spaceship = document.querySelector('.spaceship');
+        const fires = Array.from(spaceship.contentDocument.querySelectorAll('[data-name=fire]'));
         const title = document.querySelector('.title');
 
         gsap.set(land, {
             height: document.querySelector('.land').offsetHeight
         });
 
-        gsap.set(spaceship_wrapper, {
-            height: spaceship.offsetHeight,
-            width: spaceship.offsetWidth / spaceship.dataset.frames
+        gsap.set(fires, {
+            display: 'none'
         });
 
-        /*gsap.set(land_1, {
+        gsap.set(spaceship_wrapper, {
+            height: spaceship.offsetHeight,
+            width: spaceship.offsetWidth / spaceship.dataset.frames,
+            top: '-60%',
+            left: '50%',
+            x: '-50%'
+        });
+
+        gsap.set(land, {
             opacity: 0
         });
 
-        gsap.set(land_2, {
+        gsap.set(arrive, {
             y: '100%'
         });
 
-        const tl_land_1 = gsap.timeline({
+        const tl_departure = gsap.timeline({
             scrollTrigger: {
                 trigger: '.main',
-                start: 'top top',
+                start: '1% top',
                 end: '20% top',
                 scrub: true,
             },
         });
 
-        tl_land_1.to(land_1, { opacity: 1, duration: 1 })
-        .to(land_1, { y: '100%', duration: 1 })
+        tl_departure.to(land, { opacity: 1, duration: 1 })
+            .to(departure, { y: '100%', duration: 1 })
 
-        gsap.to(land_2, {
+        gsap.to(arrive, {
             scrollTrigger: {
                 trigger: 'main',
                 start: '75% center',
@@ -59,7 +67,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 //markers: true
             },
             y: '0%',
-        });*/
+        });
 
         gsap.to(title, {
             scrollTrigger: {
