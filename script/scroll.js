125b471c621954506a457baf3e9c3686a5bcadcd
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-04-16 12:34:52 +0200

    update

--- script/scroll.js
+++ script/scroll.js
@@ -1,44 +1,59 @@
 gsap.registerPlugin(ScrollTrigger, Observer);
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM loaded");
 
-    console.log('DOM loaded');
+  window.addEventListener(
+    "load",
+    () => {
+      if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+      ) {
+        // true for mobile device
+        
+      } else {
+        // false for not mobile device
+      }
 
-    window.addEventListener('load', () => {
+      // GRAPHIC ELEMENTS
 
-        // GRAPHIC ELEMENTS
+      const scroller = document.querySelector('.scroll-wrapper');
+      const main = document.querySelector(".scrollbar");
+      const departure = document
+        .querySelector(".land")
+        .contentDocument.getElementById("land-1");
+      const arrive = document
+        .querySelector(".land")
+        .contentDocument.getElementById("land-2");
 
-        //const scroller = document.querySelector('.scroll-wrapper');
-        const main = document.querySelector('.scrollbar');
-        const departure = document.querySelector('.land').contentDocument.getElementById('land-1');
-        const arrive = document.querySelector('.land').contentDocument.getElementById('land-2');
-
-        /*gsap.set(departure, {
+      /*gsap.set(departure, {
             opacity: 1
         });*/
 
-        gsap.set(arrive, {
-            y: '100%'
-        });          
+      gsap.set(arrive, {
+        y: "100%",
+      });
 
-        // MAIN TIMELINE
-        const tl = gsap.timeline({
-            scrollTrigger: {
-                //scroller: scroller,
-                id: 'main',
-                trigger: main,
-                start: 'top top',
-                end: 'bottom bottom',
-                invalidateOnRefresh: true,
-                scrub: 4,
-            },
-        });
+      // MAIN TIMELINE
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          scroller: scroller,
+          id: "main",
+          trigger: main,
+          start: "top top",
+          end: "bottom bottom",
+          invalidateOnRefresh: true,
+          scrub: 4,
+        },
+      });
 
-        tl
-            // .5 UNIT
-            .to(departure, { y: '100%', duration: 1 })
-            .to(arrive, { y: '0%', duration: 1 })
-
-    }, false);
-
+      tl
+        // .5 UNIT
+        .to(departure, { y: "100%", duration: 1 })
+        .to(arrive, { y: "0%", duration: 1 });
+    },
+    false
+  );
 });
