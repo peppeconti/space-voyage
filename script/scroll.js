02ce196a897b345219086f31e848b69ecf98e943
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-04-17 19:11:40 +0200

    update

--- script/scroll.js
+++ script/scroll.js
@@ -6,10 +6,6 @@ document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener(
     "load",
     () => {
-      const getElementScrollbarWidth = (element) => {
-        return element.offsetWidth - element.clientWidth;
-      };
-
       // GRAPHIC ELEMENTS
 
       const scroller = document.querySelector(".scroll-wrapper");
@@ -22,25 +18,39 @@ document.addEventListener("DOMContentLoaded", () => {
         .querySelector(".land")
         .contentDocument.getElementById("land-2");
 
-      departure.addEventListener("click", () => alert("ciao"));
+      // FUNCTIONS
 
-      if (
-        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-          navigator.userAgent
-        )
-      ) {
-        // true for mobile device
-        console.log('mobile');
-      } else {
-        // false for not mobile device
-        gsap.set(content, {
-          width: `calc(100% - ${getElementScrollbarWidth(scroller)}px)`,
-        });
-      }
+      const adaptToScroller = () => {
+        const getElementScrollbarWidth = (element) => {
+          return element.offsetWidth - element.clientWidth;
+        };
+
+        if (
+          /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+            navigator.userAgent
+          )
+        ) {
+          // true for mobile device
+          gsap.set(content, {
+            width: "100%",
+          });
+        } else {
+          // false for not mobile device
+          gsap.set(content, {
+            width: `calc(100% - ${getElementScrollbarWidth(scroller)}px)`,
+          });
+        }
+      };
+
+      adaptToScroller();
+
+      window.addEventListener("resize", () => {
+        adaptToScroller();
+      });
 
       gsap.set(departure, {
-            opacity: 1
-        });
+        opacity: 1,
+      });
 
       gsap.set(arrive, {
         y: "100%",
@@ -63,6 +73,20 @@ document.addEventListener("DOMContentLoaded", () => {
         // .5 UNIT
         .to(departure, { y: "100%", duration: 1 })
         .to(arrive, { y: "0%", duration: 1 });
+
+      // OBSERVER
+
+      Observer.create({
+        onDown: (self) => {
+          
+        },
+        onUp: (self) => {
+          const top = scroller.scrollTop;
+          console.log(top + self.y);
+          console.log(top);
+          gsap.to(scroller, {duration: 1, scrollTo: {y: top + self.y}});
+        },
+      });
     },
     false
   );
