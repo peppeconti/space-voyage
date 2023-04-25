ebec2e8b6238f391cc3c2c104b82fdaed656667d
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-04-25 08:44:11 +0200

    update

--- script/scroll.js
+++ script/scroll.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const main = document.querySelector(".scrollbar");
       const departure = document.getElementById("departure");
       const arrive = document.getElementById("arrive");
-      const title = document.querySelector('.title');
+      const title = document.querySelector(".title");
       const spaceship = document.querySelector(".spaceship");
       const spaceship_frames = document.querySelector(".spaceship-frames");
 
@@ -43,6 +43,31 @@ document.addEventListener("DOMContentLoaded", () => {
       gsap.set(arrive, {
         y: "100%",
       });
+
+      const meteors_tl = gsap
+        .timeline()
+        .to(spaceship, { rotate: 5, x: 10, ease: ease1, duration: 0.5 })
+        .to(spaceship, { rotate: 0, x: 0, ease: ease1, duration: 0.5 })
+        .to(
+          spaceship,
+          {
+            bottom: () => `calc(50% - ${spaceship.offsetHeight / 2 + 50}px)`,
+            ease: ease2,
+            duration: 1,
+          },
+          "-=1"
+        )
+        .to(spaceship, { rotate: -5, x: -25, ease: ease1, duration: 1 })
+        .to(
+          spaceship,
+          {
+            bottom: () => `calc(50% - ${spaceship.offsetHeight / 2}px)`,
+            ease: ease2,
+            duration: 0.5,
+          },
+          "-=1"
+        )
+        .to(spaceship, { rotate: 0, x: 0, ease: ease1, duration: 0.5 });
 
       // MAIN TIMELINE
       const tl = gsap.timeline({
@@ -74,6 +99,7 @@ document.addEventListener("DOMContentLoaded", () => {
           duration: 1,
         })
         .to(departure, { y: "100%", duration: 1 }, "-=1")
+        .add(meteors_tl, "-=4.6")
         // 2 UNIT
         .to(arrive, { y: "0%", duration: 1 }, "+=1")
         // .5 UNIT
